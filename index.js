f1f4875b938855208e0521ef3f96aff49cfc1378
JRuggy <jastonruggy@icloud.com>
2024-01-21 18:42:32 +0300

    rest crude

--- index.js
+++ index.js
@@ -30,19 +30,48 @@ app.get("/books", (req, res) => {
     })
 })
 
-
 // for posting books
 app.post("/books", (req, res) => {
-    const q = "insert into books (`title`,`description`,`cover`) values(?)"
+    const q = "insert into books (`title`,`description`,`cover`,`price`) values(?)"
     const values = [
         req.body.title,
         req.body.description,
         req.body.cover,
+        req.body.price,
     ]
 
     db.query(q, [values], (err, data) => {
         if (err) return res.json(err)
         return res.json("Books has been created successfully")
+    })
+})
+
+// endpoint for deleting books
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "delete from books where id = ?"
+
+    db.query(q, [bookId], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Book has been deleted successfully");
+    })
+})
+
+// endpoint for updating books
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "update books set `title` = ?, `description`=?, `price` = ?, `cover` = ? where id = ?"
+
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.cover,
+        req.body.price,
+    ]
+
+    db.query(q, [...values, bookId], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Book has been updated successfully");
     })
 })
 
